aaed4933bcc3c039de536e0978a0fbc1e2f80c42
Roméo GBONGBON <romeo.gbongbon@gmail.com>
2025-01-12 21:24:19 +0100

    add OpenApi documentation

--- src/main/java/fr/pathinov/bootcamp/controllers/ProductController.java
+++ src/main/java/fr/pathinov/bootcamp/controllers/ProductController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.pathinov.bootcamp.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
--- src/main/java/fr/pathinov/bootcamp/services/ProductService.java
+++ src/main/java/fr/pathinov/bootcamp/services/ProductService.java
@@ -3,15 +3,11 @@ package fr.pathinov.bootcamp.services;
 import org.springframework.stereotype.Service;
 
 import fr.pathinov.bootcamp.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
-
+    
     private final ProductRepository productRepository;
-
-    public ProductService(ProductRepository productRepository)
-    {
-        this.productRepository = productRepository;
-    }
-
 }
